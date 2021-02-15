cb7469b5726bde1d7e34bcb3ad54cc949c770ced
tmtocb <kittz.esport@gmail.com>
2021-02-15 19:28:28 +0100

    add devise User model with custom columns, follow TDD

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe '#columns' do
+    it { is_expected.to have_db_column(:email) }
+    it { is_expected.to have_db_column(:encrypted_password) }
+    it { is_expected.to have_db_column(:username) }
+    it { is_expected.to have_db_column(:name) }
+    it { is_expected.to have_db_column(:website) }
+    it { is_expected.to have_db_column(:bio) }
+    it { is_expected.to have_db_column(:phone) }
+    it { is_expected.to have_db_column(:gender) }
+  end
+end
