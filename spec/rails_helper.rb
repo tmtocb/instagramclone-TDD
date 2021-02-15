673acb1ce255a31a35eabd66e3c8892faf17756a
tmtocb <kittz.esport@gmail.com>
2021-02-15 18:05:14 +0100

    add database-cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,4 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'database_cleaner'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
@@ -61,6 +61,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
