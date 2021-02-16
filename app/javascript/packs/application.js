5d9ebd35ded53e943db312460286a8edcfac8b4f
tmtocb <kittz.esport@gmail.com>
2021-02-16 10:39:03 +0100

    add font-awesome via webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 require("stylesheets/application.scss")
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap' 
+import "@fortawesome/fontawesome-free/css/all"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
