a8eee42bbb38cdd114b6a85e70b9b3c2328887d6
tmtocb <kittz.esport@gmail.com>
2021-02-18 12:15:05 +0100

    add toastr.js for custom, non-blocked flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ require("channels")
 require("stylesheets/application.scss")
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap' 
-import "@fortawesome/fontawesome-free/css/all"
-
+import '@fortawesome/fontawesome-free/css/all'
+global.toastr = require('toastr')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
