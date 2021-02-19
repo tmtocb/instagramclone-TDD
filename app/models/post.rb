8189c6ad7373ae450b2e2776de8f6da71f17dd36
tmtocb <kittz.esport@gmail.com>
2021-02-19 01:50:32 +0100

    add Post model validations, follow TDD

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  validates :description, presence: true
+  validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe Post, type: :model do
     it { should belong_to(:user) }
   end
 
+  describe '#validations' do
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:image) }
+  end
+
   describe '#attachments' do
     it { should have_one_attached(:image) }
   end
